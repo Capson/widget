7c5305622b21dbccb13e94a557ae4b23fdbf0fd1
simar525 <ssimarpreet11@gmail.com>
2023-07-17 00:32:51 +0530

    cloozo got added18

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -45,7 +45,7 @@ export const Badge = (props: Props) => {
       "font-size": '13px',
       position: 'absolute',
       bottom: 0,
-      padding: '10px',
+      padding: '20px 10px 10px',
       margin: 'auto',
       width: '100%',
       "text-align": 'center',
