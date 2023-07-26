04eac6efe3bd4ed93e1f3f800e5204dc116f6f01
simar525 <ssimarpreet11@gmail.com>
2023-07-27 01:26:22 +0530

    cloozo got added51

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Cloozo</span>
+        <span> cloozo.com</span>
       </a>
     </span>
   )
