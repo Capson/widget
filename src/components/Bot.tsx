38120bcdb3fc301a277c553394af5814400672d4
simar525 <ssimarpreet11@gmail.com>
2023-08-03 16:18:19 +0530

    cloozo got added100

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -291,14 +291,17 @@ export const Bot = (props: BotProps & { class?: string }) => {
                     <img src={props.chatflowConfig.imagePath} alt='Chat Icon' style={{width: '60px', height: '60px', marginTop: '10px'}}/>
                      }
                      </div>
+                     
+                     <div class="image-container">
+                                        <img src="https://i.ibb.co/5LyLXk5/ddd.png" alt="Your Alt Text" style={{width: '200px', height: '100px'}}/>
+                                        </div>    
+
                <div class="flex w-full h-full justify-center">
                     <div style={{ "padding-bottom": '100px' }} ref={chatContainer} class="overflow-y-scroll min-w-full w-full min-h-full px-3 pt-10 relative scrollable-container chatbot-chat-view scroll-smooth">
                         <For each={[...messages()]}>
                             {(message, index) => (
                                 <>
-                                       <div class="image-container">
-                                        <img src="https://i.ibb.co/5LyLXk5/ddd.png" alt="Your Alt Text" style={{width: '200px', height: '100px'}}/>
-                                        </div> 
+                                       
                                     {message.type === 'userMessage' && (
                                         <GuestBubble
                                             message={message.message}
