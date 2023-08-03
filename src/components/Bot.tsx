ddebac71ac766387ba40f1df214f1e190fd4406c
simar525 <ssimarpreet11@gmail.com>
2023-08-03 16:47:09 +0530

    cloozo got added109

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -285,20 +285,12 @@ export const Bot = (props: BotProps & { class?: string }) => {
 
     return (
         <>
+                    <div ref={botContainer} class={'relative flex w-full h-full text-base overflow-hidden bg-cover bg-center flex-col items-center chatbot-container ' + props.class}>
+                    
+                    
                     <div class="image-container">
                                         <img src="https://i.ibb.co/5LyLXk5/ddd.png" alt="Your Alt Text" style={{width: '100px', height: '50px'}}/>
                         </div>
-
-
-                    <div ref={botContainer} class={'relative flex w-full h-full text-base overflow-hidden bg-cover bg-center flex-col items-center chatbot-container ' + props.class} 
-                    style={{ height: 'calc(100% + 300px)' }}>
-                    
-                    
-                    <div class="flex items-start">
-                    {typeof props.chatflowConfig?.imagePath === 'string' &&
-                    <img src={props.chatflowConfig.imagePath} alt='Chat Icon' style={{width: '60px', height: '60px', marginTop: '10px'}}/>
-                     }
-                     </div>
 
                      
 
